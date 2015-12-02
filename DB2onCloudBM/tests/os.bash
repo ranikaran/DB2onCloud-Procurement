4e2df2856a372a12fb769204bef0154cee72abb9
ranikaran <ranikaran@us.ibm.com>
2015-12-02 13:01:13 -0800

    Update os.bash

--- DB2onCloudBM/tests/os.bash
+++ DB2onCloudBM/tests/os.bash
@@ -4,7 +4,7 @@ preflight_test () {
 
 os=$2
 osv=$3
-  # test OS; should be debian 6 or 7 or ubuntu 12.04 or 14.04
+  # test OS; should be Red Hat version 7.1
   if [[ -e /etc/redhat-release ]];
   then
     ver=$(cat /etc/redhat-release | awk '{print $7}')
